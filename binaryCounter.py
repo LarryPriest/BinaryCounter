c9e3d4eaffc29811a5dfdb7ecdc923f8195649b0
LarryPriest <larrytpriest@gmail.com>
2021-06-17 21:03:30 -0600

    Added some control and expanded to 16 bits.

--- binaryCounter.py
+++ binaryCounter.py
@@ -13,8 +13,12 @@ import board
 import digitalio
 import time
 
+bits = 15
+delay = 0.25
+count = 0xffff
 lowByteLed = []
 
+# Low byte
 lowByteLed.append(digitalio.DigitalInOut(board.GP0))
 lowByteLed[0].direction = digitalio.Direction.OUTPUT
 lowByteLed.append(digitalio.DigitalInOut(board.GP1))
@@ -32,23 +36,41 @@ lowByteLed[6].direction = digitalio.Direction.OUTPUT
 lowByteLed.append(digitalio.DigitalInOut(board.GP7))
 lowByteLed[7].direction = digitalio.Direction.OUTPUT
 
-for i in range(256):
+# High byte
+lowByteLed.append(digitalio.DigitalInOut(board.GP8))
+lowByteLed[8].direction = digitalio.Direction.OUTPUT
+lowByteLed.append(digitalio.DigitalInOut(board.GP9))
+lowByteLed[9].direction = digitalio.Direction.OUTPUT
+lowByteLed.append(digitalio.DigitalInOut(board.GP10))
+lowByteLed[10].direction = digitalio.Direction.OUTPUT
+lowByteLed.append(digitalio.DigitalInOut(board.GP11))
+lowByteLed[11].direction = digitalio.Direction.OUTPUT
+lowByteLed.append(digitalio.DigitalInOut(board.GP12))
+lowByteLed[12].direction = digitalio.Direction.OUTPUT
+lowByteLed.append(digitalio.DigitalInOut(board.GP13))
+lowByteLed[13].direction = digitalio.Direction.OUTPUT
+lowByteLed.append(digitalio.DigitalInOut(board.GP14))
+lowByteLed[14].direction = digitalio.Direction.OUTPUT
+lowByteLed.append(digitalio.DigitalInOut(board.GP15))
+lowByteLed[15].direction = digitalio.Direction.OUTPUT
+
+# main loop
+for i in range(count):
     byte = bin(i)[2:]  # leading zeros missing
 #     byte = format(i, '08b') # gives full 8 bits
     print(byte)  # reality check
     byte_list = []
     for bit in iter(byte):
         byte_list.append(bit)
-    list.reverse(byte_list)  #gits the list to the right
-    bit_count = 7  # reverse if u wired the other way up
+    list.reverse(byte_list)  # gits the list to the right
+    bit_count = bits  # reverse if u wired the other way up
     for i in iter(byte_list):
         if i == '1':
             lowByteLed[bit_count].value = True
         else:
             lowByteLed[bit_count].value = False
         bit_count -= 1
-    time.sleep(0.25)
-   
+    time.sleep(delay)
+
 for i in range(len(lowByteLed)):
     lowByteLed[i].deinit()
-        
