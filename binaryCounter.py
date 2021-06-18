2f6ae092553d78777274596a56c7865a5e9c0b38
LarryPriest <larrytpriest@gmail.com>
2021-06-17 23:36:19 -0600

    Fixed the Setup of the LED's to save space and typing.

--- binaryCounter.py
+++ binaryCounter.py
@@ -1,10 +1,11 @@
 '''binaryCounter.py
 June 14, 2021
-My binary counter. Displays binary count-up on a string of 8 LED's.
+Author: Larry Priest email: larrytpriest@gmail.com
+My binary counter. Displays binary count-up on a string of n LED's.
 Hardware:
     RaspberryPi Pico
     breadboard or proto/perf board
-    8 220 ohm resistors, 8 5mm red LED's
+    n 220 ohm resistors, n 5mm red LED's
 pin 1/GP0 is the High bit in my wiring.
 '''
 
@@ -13,46 +14,17 @@ import board
 import digitalio
 import time
 
-bits = 15
-delay = 0.25
-count = 0xffff
+bits = 15  # start at zero
+delay = 0.25  # seconds
+count = 0xffff  # any interger (hex 'cause it makes sense)
 lowByteLed = []
 
-# Low byte
-lowByteLed.append(digitalio.DigitalInOut(board.GP0))
-lowByteLed[0].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP1))
-lowByteLed[1].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP2))
-lowByteLed[2].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP3))
-lowByteLed[3].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP4))
-lowByteLed[4].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP5))
-lowByteLed[5].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP6))
-lowByteLed[6].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP7))
-lowByteLed[7].direction = digitalio.Direction.OUTPUT
-
-# High byte
-lowByteLed.append(digitalio.DigitalInOut(board.GP8))
-lowByteLed[8].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP9))
-lowByteLed[9].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP10))
-lowByteLed[10].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP11))
-lowByteLed[11].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP12))
-lowByteLed[12].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP13))
-lowByteLed[13].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP14))
-lowByteLed[14].direction = digitalio.Direction.OUTPUT
-lowByteLed.append(digitalio.DigitalInOut(board.GP15))
-lowByteLed[15].direction = digitalio.Direction.OUTPUT
+# Setup LED list
+subcommand = 'lowByteLed.append(digitalio.DigitalInOut(board.GP'
+for i in range(bits+1):
+    fullcommand = subcommand + str(i) + '))'
+    exec(fullcommand)
+    lowByteLed[i].direction = digitalio.Direction.OUTPUT
 
 # main loop
 for i in range(count):
