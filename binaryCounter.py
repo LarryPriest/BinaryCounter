4325f06d81b35ab379bf6f904421c518322cdceb
LarryPriest <larrytpriest@gmail.com>
2021-06-29 15:07:33 -0600

    Classified and updated sume error checking.

--- binaryCounter.py
+++ binaryCounter.py
@@ -9,40 +9,55 @@ Hardware:
 pin 1/GP0 is the High bit in my wiring.
 '''
 
-
 import board
 import digitalio
 import time
 
-bits = 15  # start at zero
+TOTALBITS = 15  # start at zero
 delay = 0.25  # seconds
 count = 0xffff  # any interger (hex 'cause it makes sense)
-lowByteLed = []
+LEDbits = []
 
-# Setup LED list
-subcommand = 'lowByteLed.append(digitalio.DigitalInOut(board.GP'
-for i in range(bits+1):
-    fullcommand = subcommand + str(i) + '))'
-    exec(fullcommand)
-    lowByteLed[i].direction = digitalio.Direction.OUTPUT
 
-# main loop
-for i in range(count):
-    byte = bin(i)[2:]  # leading zeros missing
-#     byte = format(i, '08b') # gives full 8 bits
-    print(byte)  # reality check
-    byte_list = []
-    for bit in iter(byte):
-        byte_list.append(bit)
-    list.reverse(byte_list)  # gits the list to the right
-    bit_count = bits  # reverse if u wired the other way up
-    for i in iter(byte_list):
-        if i == '1':
-            lowByteLed[bit_count].value = True
-        else:
-            lowByteLed[bit_count].value = False
-        bit_count -= 1
-    time.sleep(delay)
+class binaryCounter():
+    def __init__(self):
+        # Setup LED list
+        subcommand = 'LEDbits.append(digitalio.DigitalInOut(board.GP'
+        for i in range(TOTALBITS+1):
+            fullcommand = subcommand + str(i) + '))'
+            exec(fullcommand)
+            LEDbits[i].direction = digitalio.Direction.OUTPUT
 
-for i in range(len(lowByteLed)):
-    lowByteLed[i].deinit()
+    # main loop
+    def bicounter():
+        for i in range(count):
+            counterBits = bin(i)[2:]  # leading zeros missing
+        #     byte = format(i, '08b') # gives full 8 bits
+            print(counterBits)  # reality check
+            byte_list = []
+            for bit in iter(counterBits):
+                byte_list.append(bit)
+            list.reverse(byte_list)  # gits the list to the right
+            bit_count = TOTALBITS  # reverse if u wired the other way up
+            for i in iter(byte_list):
+                if i == '1':
+                    LEDbits[bit_count].value = True
+                else:
+                    LEDbits[bit_count].value = False
+                bit_count -= 1
+            time.sleep(delay)
+
+    def bicounterClose():
+        print('Closing outputs.')
+        for i in range(len(LEDbits)):
+            LEDbits[i].deinit()
+
+
+if __name__ == '__main__':
+    binaryCounter()
+    try:
+        binaryCounter.bicounter()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        binaryCounter.bicounterClose()
